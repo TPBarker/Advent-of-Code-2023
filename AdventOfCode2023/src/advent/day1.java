9ca1cfc7dca8572517fd4812ccb7d2b1e44a4b22
TPBarker <tom.peter.barker@gmail.com>
2023-12-17 18:29:22 +1000

    Finishing touches on day1

--- AdventOfCode2023/src/advent/day1.java
+++ AdventOfCode2023/src/advent/day1.java
@@ -75,91 +75,19 @@ public class day1 {
     }
     
     /**
-     * Recursively check through a provided String and replace all fully spelled
-     * out numeral words with numeric digits. Because of the way the challenge
-     * works, we must only replace the first instance of each numeral and then
-     * re-check the word (e.g. oneight must end up as 1ight, not 18).
      * @param haystack the String to be checked
      * @return the String with replacement performed.
      */
     public String removeNumerals(String haystack) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < haystack.length(); i++) {
-            builder.append(Character.toString(haystack.charAt(i)));
-
-            if (builder.toString().contains("one")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("one"), newStack.indexOf("one") + 3);
-                newStack.append('1');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }
-            if (builder.toString().contains("two")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("two"), newStack.indexOf("two") + 3);
-                newStack.append('2');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }    
-            if (builder.toString().contains("three")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("three"), newStack.indexOf("three") + 5);
-                newStack.append('3');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }     
-            if (builder.toString().contains("four")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("four"), newStack.indexOf("four") + 4);
-                newStack.append('4');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }     
-            if (builder.toString().contains("five")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("five"), newStack.indexOf("five") + 4);
-                newStack.append('5');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }     
-            if (builder.toString().contains("six")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("six"), newStack.indexOf("six") + 3);
-                newStack.append('6');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }     
-            if (builder.toString().contains("seven")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("seven"), newStack.indexOf("seven") + 5);
-                newStack.append('7');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }     
-            if (builder.toString().contains("eight")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("eight"), newStack.indexOf("eight") + 5);
-                newStack.append('8');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }     
-            if (builder.toString().contains("nine")) {
-                StringBuilder newStack = new StringBuilder(haystack);
-                newStack.delete(newStack.indexOf("nine"), newStack.indexOf("nine") + 4);
-                newStack.append('9');
-                haystack = newStack.toString();
-                builder = new StringBuilder();
-                haystack = removeNumerals(haystack);
-            }     
-        }
+        haystack = haystack.replaceAll("one", "one1one");
+        haystack = haystack.replaceAll("two", "two2two");
+        haystack = haystack.replaceAll("three", "three3three");
+        haystack = haystack.replaceAll("four", "four4four");
+        haystack = haystack.replaceAll("five", "five5five");
+        haystack = haystack.replaceAll("six", "six6six");
+        haystack = haystack.replaceAll("seven", "seven7seven");
+        haystack = haystack.replaceAll("eight", "eight8eight");
+        haystack = haystack.replaceAll("nine", "nine9nine");
         return haystack; 
     }
 }
